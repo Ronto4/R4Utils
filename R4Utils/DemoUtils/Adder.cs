ec88171489803572278c913448e61a52b7d6bba3
Ronto4 <34981840+Ronto4@users.noreply.github.com>
2021-05-09 01:10:06 +0200

    Add demo test

---
+++ R4Utils/DemoUtils/Adder.cs
@@ -0,0 +1,10 @@
+namespace R4Utils.DemoUtils
+{
+    /// <summary>
+    /// This will only exist to test the tests.
+    /// </summary>
+    public static class Adder
+    {
+        public static int Add(int left, int right) => left + right;
+    }
+}
